eec25c953195c912fa3026cd88a556b6aa3dac18
darshan4694 <darshan.shah4694@gmail.com>
2017-08-11 15:26:39 -0500

    Added 'morgan' to log the requests into the console

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const jsonParser = require('body-parser').json;
+const logger = require('morgan');
 
 const routes = require('./routes');
 const app = express();
 var port = process.env.PORT || 3000;
 
+app.use(logger('dev'));
 app.use(jsonParser());
 
 app.use('/questions', routes);
