8276cd301f19e513a56fd59f35939695ced06127
darshan4694 <darshan.shah4694@gmail.com>
2017-08-11 15:22:30 -0500

    Added request handlers for answers and votes

--- routes.js
+++ routes.js
@@ -12,13 +12,52 @@ router.post('/', (req,res) => {
         response: "You sent a POST request",
         body: req.body
     });
-})
+});
 
 
 router.get('/:id', (req,res) => {
     res.json({
         response: "You sent a GET request for an ID : " + req.params.id
     });
-})
+});
+
+router.get('/:qID/answers', (req, res) => {
+    res.json({
+        response: "You sent a GET request for all the answers of the question ID : " + req.params.qID
+    });
+});
+
+router.post('/:qID/answers', (req, res) => {
+    res.json({
+        response: "You sent a POST request for the answer of the question ID : " + req.params.qID,
+        body: req.body
+    });
+});
+
+router.put('/:qID/answers/:aID', (req, res) => {
+    res.json({
+        response: "You sent a PUT request for the answer of the question ID : " + req.params.qID,
+        questionID: req.params.qID,
+        answerID: req.params.aID,
+        body: req.body
+    });
+});
+
+router.delete('/:qID/answers/:aID', (req, res) => {
+    res.json({
+        response: "You sent a DELETE request for the answer of the question ID : " + req.params.qID,
+        questionID: req.params.qID,
+        answerID: req.params.aID
+    });
+});
+
+router.post('/:qID/answers/:aID/vote-:dir', (req, res) => {
+    res.json({
+        response: "You sent a POST request for vote" + req.params.dir,
+        questionID: req.params.qID,
+        answerID: req.params.aID,
+        vote: req.params.dir
+    });
+});
 
 module.exports = router;
