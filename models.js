17a22e134f9f9afef4b4e5ae2365856f1c39a476
darshan4694 <darshan.shah4694@gmail.com>
2017-08-11 19:12:30 -0500

    Set up mongoose method for storing votes and updatedAt time.

---
+++ models.js
@@ -0,0 +1,58 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+var sortAnswer = function(a, b) {
+    if(b.votes === a.votes){
+        return b.updatedAt - a.updatedAt;
+    }
+    return b.votes - a.votes;
+};
+
+const AnswerSchema = new Schema({
+    text: String,
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    updatedAt: {
+        type: Date,
+        default: Date.now
+    },
+    votes: { 
+        type: Number,
+        default: 0
+    }
+});
+
+AnswerSchema.method('update', (updates, callback) => {
+    Object.assign(this, updates, {updatedAt: new Date()});
+    this.parent.save(callback);
+});
+
+AnswerSchema.method('vote', (vote, callback) => {
+   if(vote === 'up'){
+       this.votes += 1;
+   } else {
+       this.votes -= 1;
+   }
+   this.parent.save(callback);
+});
+
+const QuestionSchema = new Schema({
+    text: String,
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    answers: [AnswerSchema]
+});
+
+QuestionSchema.pre('save', (next) => {
+    this.answers.sort(sortAnswer);
+    next();
+});
+
+const Question = mongoose.model('Question', QuestionSchema);
+
+module.exports = Question;
